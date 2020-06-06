ce0945186a8def7f463dfc9c14aa6bb749b522e8
Juan Patrón <josejuanpatron1630@gmail.com>
2020-06-06 17:28:22 -0500

    Hola mundo en C#   xd

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+// Esta librería sirve para poder hacer peticiones http
+using Newtonsoft.Json;
+// esta librerira nos ayuda a poder leer los json 
+
+namespace holaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string API_URL =  "http://jsonplaceholder.typicode.com/posts?_limit=5";
+
+            var client = new WebClient();
+            // creamos una instancia de un cliente web, para poder hacer peticiones http
+
+           var respJson = client.DownloadString(API_URL);
+            // hacemos la peticion
+
+            Console.WriteLine( respJson );
+        }
+    }
+}
+ 
