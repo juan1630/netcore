eba8332e9eeec322a588e851b63ff0d788146333
Juan Patrón <josejuanpatron1630@gmail.com>
2020-06-08 19:58:52 -0500

    Se vio la sintaxis basica

--- Program.cs
+++ Program.cs
@@ -7,17 +7,27 @@ using Newtonsoft.Json;
 namespace holaMundo
 {
     class Program
+    // estos son los identificadores
+    // solo usan letras, numero y guiones bajos
+    // deben de iniciar con letras y no con numeros
     {
-        static void Main(string[] args)
-        {
+        static void Main(string[] args) {
             string API_URL =  "http://jsonplaceholder.typicode.com/posts?_limit=5";
+
+            // comentarios en C#
+            /*
+                Invalidamos este bloque de codigo
+            
+            */
 
             var client = new WebClient();
             // creamos una instancia de un cliente web, para poder hacer peticiones http
 
-           var respJson = client.DownloadString(API_URL);
-            // hacemos la peticion
+            var respJson = client.DownloadString(API_URL);
+            // hacemos la peticion y la guardamos en la variable
 
+
+            // imprime en la consola la resp
             Console.WriteLine( respJson );
         }
     }
